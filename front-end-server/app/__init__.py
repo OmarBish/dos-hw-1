4f41b5fa2b3b5f69b85ae38a74d6b65e100dfdab
Omar Bishtawi <omar@dawsat.com>
2020-02-25 18:29:59 +0200

    add cors to front end server

--- front-end-server/app/__init__.py
+++ front-end-server/app/__init__.py
@@ -1,11 +1,14 @@
 # third-party imports
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 # local imports
 from config import app_config
 
 # Initialize application
 app = Flask(__name__, instance_relative_config=True)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 #get routes
 from app import routes
