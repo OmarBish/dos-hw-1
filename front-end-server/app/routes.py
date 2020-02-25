8af824b54a422700daee00218f9fad38511d2136
Omar Bishtawi <omar@dawsat.com>
2020-02-25 17:42:08 +0200

    add book object

--- front-end-server/app/routes.py
+++ front-end-server/app/routes.py
@@ -28,7 +28,7 @@ def Books():
         # TODO:- decode the json and send the result to the user
         # result.text
         data={'id':1,'title':'lorem ipsum','amount':5}
-        return buildResponse(message='Record added successfully',data=data ,code=201)
+        return jsonify(data) ,201
     #list all books
     elif request.method == 'GET':
         title = request.args.get('title') 
@@ -43,4 +43,77 @@ def Books():
         res =[]
         res.append({'id':1,'title':'lorem ipsum','amount':5})
         res.append({'id':2,'title':'lorem ipsum','amount':0})
-        return buildResponse(message='Records fetched successfully',data=res ,code=200)
+        return jsonify(res) ,200
+
+# Book Object 
+@app.route('/books/<book_id>',methods=['GET', 'POST'])
+def Book(book_id):
+    #add new book
+    if request.method == 'POST':
+        data = request.get_json()
+
+        if data['operation'] == 'buy':
+            sqlite_insert_query = "SELECT * FROM books where id = "+ book_id
+             # TODO:- add the order srver url
+            # result = requests.get('http://example.com')
+            # TODO:- decode the json and send the result to the user
+            # result.text
+            records = [{'id':1,'title':'lorem ipsum','amount':5}]
+            if(len(records) == 0):
+                res = {
+                    'message': 'Record Not Found'
+                }
+                return jsonify(res) ,404
+            book = records[0]
+
+            if(book[2] == 0):
+                res = {
+                    'message': 'Out of stock'
+                }
+                return jsonify(res) ,410
+
+            newAmount = book[2] - 1
+            sql_update_query = "UPDATE books SET amount = "+ str(newAmount) +" WHERE id = "+book_id
+
+            # TODO:- add the order srver url
+            # result = requests.get('http://example.com')
+            # TODO:- decode the json and send the result to the user
+            # result.text
+            
+            res = {
+                'id' : book[0],
+                'title' : book[1],
+                'amount' : newAmount,
+            }
+            return jsonify(res) ,200
+
+        else:
+             res = {
+                'message': 'Unsupported operation'
+            }
+            return jsonify(res) ,405
+            
+
+    #list book object data
+    elif request.method == 'GET':
+        sqlite_insert_query = "SELECT * FROM books where id = "+ book_id
+         # TODO:- add the order srver url
+        # result = requests.get('http://example.com')
+        # TODO:- decode the json and send the result to the user
+        # result.text
+
+        records = [{'id':1,'title':'lorem ipsum','amount':5}]
+        if(len(records) == 0):
+            res = {
+                'message': 'Record Not Found'
+            }
+            return jsonify(res) ,404
+
+        
+        row = records[0]
+        res = {'id':row[0],'title':row[1],'amount':row[2]}
+        
+        return jsonify(res) ,200
+
+
+
